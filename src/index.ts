5e36aacae68a6502245d4bc394b4a03a8a258506
dmun <davidru01@live.nl>
2024-04-12 01:14:16 +0200

    changes

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Client, Events, GatewayIntentBits, InteractionType } from "discord.js";
 import { config } from "./config";
 import { deployCommands } from "./deploy-commands";
 import { commands } from "./commands";
+import { INTEGER, STRING, Sequelize } from "sequelize";
 
 const client = new Client({
   intents: [
@@ -12,7 +13,24 @@ const client = new Client({
   ],
 });
 
+const sequelize = new Sequelize("database", "user", "passwsord", {
+  host: "localhost",
+  dialect: "sqlite",
+  logging: false,
+  storage: "database.sqlite",
+});
+
+const Tags = sequelize.define("tags", {
+  username: STRING,
+  usage_count: {
+    type: INTEGER,
+    defaultValue: 0,
+    allowNull: false,
+  },
+});
+
 client.once(Events.ClientReady, readyClient => {
+  Tags.sync();
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
@@ -35,9 +53,30 @@ client.on(Events.InteractionCreate, async interaction => {
       break;
     case InteractionType.ModalSubmit:
       if (interaction.isModalSubmit()) {
-        interaction.reply(
-          `set to ${interaction.fields.getTextInputValue("input")}`,
-        );
+        const time = interaction.fields.getTextInputValue("input");
+
+        const tag = await Tags.findOne({
+          where: { username: interaction.user.username },
+        });
+
+        if (tag) {
+          tag.increment("usage_count");
+          console.log("log", tag.get("usage_count"));
+        }
+
+        try {
+          const tag = await Tags.create({
+            username: interaction.user.username,
+          });
+        } catch (error: any) {
+          if (error.name === "SequelizeUniqueConstraintError") {
+            interaction.reply("That tag already exists.");
+          }
+
+          interaction.reply("Something went wrong with adding a tag.");
+        }
+
+        interaction.reply(`set to ${time}`);
       }
       break;
   }
