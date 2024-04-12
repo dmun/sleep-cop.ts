44482d93f8928d143724eeafcfd42278132e7e5c
dmun <davidru01@live.nl>
2024-04-12 21:01:09 +0200

    progijeap

--- index.ts
+++ index.ts
@@ -1,4 +1,11 @@
-import { Client, Events, GatewayIntentBits, InteractionType } from "discord.js";
+import {
+  Client,
+  Colors,
+  Events,
+  GatewayIntentBits,
+  InteractionType,
+  PermissionFlagsBits,
+} from "discord.js";
 import { config } from "./config";
 import { deployCommands } from "./deploy-commands";
 import commands from "./commands";
@@ -16,7 +23,11 @@ const client = new Client({
 
 client.once(Events.ClientReady, readyClient => {
   Tags.sync();
+  const permissions = 39584887996432;
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+  console.log(
+    `https://discord.com/api/oauth2/authorize?client_id=${config.SLEEP_COP_CLIENT_ID}&permissions=${permissions}&scope=bot%20applications.commands`,
+  );
 });
 
 client.on(Events.GuildCreate, async guild => {
@@ -24,6 +35,32 @@ client.on(Events.GuildCreate, async guild => {
 });
 
 client.on(Events.InteractionCreate, async interaction => {
+  console.log(interaction.guildId);
+
+  const guild = client.guilds.cache.get("511184618976837672");
+
+  if (guild) {
+    console.log("actual", guild.id);
+    const bruh = guild.channels.cache
+      .filter(channel => channel.isVoiceBased())
+      .find(channel => {
+        if (channel.isVoiceBased()) {
+          channel.members.forEach(x => console.log(x.id));
+          const member = channel.members.find(
+            x => x.id === "157063395969990656",
+          );
+          member?.voice.disconnect();
+          console.log(member?.manageable);
+        }
+      });
+    console.log(bruh);
+
+    // guild.channels.cache.filter(guild => guild.members);
+    // client.guilds.cache.filter(guild =>
+    //   guild.members.cache.has(interaction.member!!.user.id),
+    // );
+  }
+
   switch (interaction.type) {
     case InteractionType.ApplicationCommand:
       const { commandName } = interaction;
@@ -44,5 +81,27 @@ client.on(Events.InteractionCreate, async interaction => {
       break;
   }
 });
+
+client.on(Events.ChannelUpdate, interaction => {
+  console.log("feopwapfeafo", interaction);
+});
+
+client.on(Events.VoiceStateUpdate, interaction => {
+  console.log("gjopeapfjeaf", interaction);
+});
+
+function checktime() {
+  const now = new Date();
+  const hour = now.getHours();
+  const minute = now.getMinutes();
+
+  console.log(`${hour}:${minute}`);
+
+  if (hour === 21 && minute === 0) {
+    console.log("bruh");
+  }
+}
+
+setInterval(checktime, 5000);
 
 client.login(config.SLEEP_COP_TOKEN);
